9f7c0f3d0acc703ab65ba5ec5b1642b6a7250b16
rezasdsju <rezaul2002104@gmail.com>
2026-09-24 09:15:33 +0600

    chore: improve navbar responsiveness

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -11,10 +11,10 @@ const Navbar = () => {
     </>
     return (
         <nav className="sticky top-0 z-50">
-            <div className="navbar bg-base-100 shadow-sm border-b-[0.5px] sm:px-10">
+            <div className="navbar bg-base-100 shadow-sm border-b-[0.5px] sm:px-3 md:px-7 lg:px-10">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <div tabIndex={0} role="button" className="btn btn-ghost sm:hidden">
                             <svg aria-label="Menu" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                         </div>
                         <ul
@@ -26,7 +26,7 @@ const Navbar = () => {
                     
                     <Link href='/' className="btn btn-ghost text-xl"><Image src={logo} alt="Logo" width={20} height={20}></Image><span >FITLOG</span></Link>
                 </div>
-                <div className="navbar-center hidden lg:flex">
+                <div className="navbar-center hidden sm:flex">
                     <ul className="menu menu-horizontal px-1">
                         {links}
                     </ul>
