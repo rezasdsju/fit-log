a5d6e3ceab7ee35c1be1b5307fa23d26a70601b5
rezasdsju <rezaul2002104@gmail.com>
2026-09-24 16:15:33 +0600

    feat: add workout context

---
+++ src/context/WorkoutContext.tsx
@@ -0,0 +1,33 @@
+import { IWorkout } from '@/types/exerciseDataTypes';
+import React, { createContext, useState } from 'react';
+
+
+interface IWorkoutContext {
+    todayPlan:IWorkout[],
+    setTodayPlan: React.Dispatch<React.SetStateAction<IWorkout[]>>,
+    saved:IWorkout[],
+    setSaved:React.Dispatch<React.SetStateAction<IWorkout[]>>
+}
+export const WorkoutContext = createContext<IWorkoutContext>({
+    todayPlan:[],
+    setTodayPlan:()=>{},
+    saved:[],
+    setSaved:()=>{}
+})
+
+
+const WorkoutProvider = ({ children }: { children: React.ReactNode }) => {
+    const [todayPlan, setTodayPlan] = useState<IWorkout[]>([])
+    const [saved, setSaved] = useState<IWorkout[]>([])
+    const sharedData = {
+        todayPlan,
+        setTodayPlan,
+        saved,
+        setSaved
+    }
+    return (
+        <WorkoutContext.Provider value={sharedData}>{children}</WorkoutContext.Provider>
+    );
+};
+
+export default WorkoutProvider;
