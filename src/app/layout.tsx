babc950647e4813cb593b204f693c0ceb69e3029
rezasdsju <rezaul2002104@gmail.com>
2026-09-24 16:18:54 +0600

    chore: mark workout as a client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
+import WorkoutProvider from "@/context/WorkoutContext";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,8 +29,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <WorkoutProvider>
         <Navbar></Navbar>
         {children}
+        </WorkoutProvider>
       </body>
     </html>
   );
