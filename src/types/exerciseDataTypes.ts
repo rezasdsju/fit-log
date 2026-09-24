f40ff860526852397e04ff8bcfce2a82d4c9f479
rezasdsju <rezaul2002104@gmail.com>
2026-09-24 11:23:46 +0600

    feat: add workout data types

---
+++ src/types/exerciseDataTypes.ts
@@ -0,0 +1,18 @@
+export interface IWorkout {
+    "id": number,
+    "name": string,
+    "image": string,
+    "muscleGroups": string[],
+    "equipment": string,
+    "difficulty": "Beginner" | "Intermediate" | "Advanced",
+    "duration": number,
+    "caloriesBurned": number,
+    "sets": number,
+    "reps": string,
+    "rating": number,
+    "description": string,
+    "instructions": string[]
+}
+
+
+
