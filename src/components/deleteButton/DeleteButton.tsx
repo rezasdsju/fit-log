7b3271ab0b2e57b419437befe620c26bcc5a4e22
rezasdsju <rezaul2002104@gmail.com>
2026-09-25 09:16:09 +0600

    fix: use functional updates when deleting workouts

--- src/components/deleteButton/DeleteButton.tsx
+++ src/components/deleteButton/DeleteButton.tsx
@@ -7,17 +7,21 @@ import { FaTimes } from "react-icons/fa";
 
 
 const DeleteButton = ({ workout }: { workout: IWorkout }) => {
-    const { todayPlan, setTodayPlan, saved, setSaved, selectedTab, markAsDone, setMarkAsDone } = useContext(WorkoutContext)
+    // const { todayPlan, setTodayPlan, saved, setSaved, selectedTab, markAsDone, setMarkAsDone } = useContext(WorkoutContext)
+    const { setTodayPlan, setSaved, selectedTab,  setMarkAsDone } = useContext(WorkoutContext)
 
     const handleDelete = () => {
         if (selectedTab==='today-plan') {
-            const remainingTodayPlan = todayPlan.filter(p => p.id !== workout.id)
-            setTodayPlan(remainingTodayPlan)
-            const remainingMarkAsDone = markAsDone.filter(m=>m!==workout.id)
-            setMarkAsDone(remainingMarkAsDone)
+            // const remainingTodayPlan = todayPlan.filter(p => p.id !== workout.id)
+            // setTodayPlan(remainingTodayPlan)
+            setTodayPlan(prev=>prev.filter(p=>p.id!==workout.id))
+            // const remainingMarkAsDone = markAsDone.filter(m=>m!==workout.id)
+            // setMarkAsDone(remainingMarkAsDone)
+            setMarkAsDone(prev=>prev.filter(m=>m!==workout.id))
         } else {
-            const remainingSaved = saved.filter(s => s.id !== workout.id)
-            setSaved(remainingSaved)
+            // const remainingSaved = saved.filter(s => s.id !== workout.id)
+            // setSaved(remainingSaved)
+            setSaved(prev=>prev.filter(s => s.id !== workout.id))
         }
     }
     return (
