0c00525dea839583c53f557d774bd1521333e749
rezasdsju <rezaul2002104@gmail.com>
2026-09-25 08:57:04 +0600

    feat: remove completed status when workout is removed from today's plan

--- src/components/deleteButton/DeleteButton.tsx
+++ src/components/deleteButton/DeleteButton.tsx
@@ -7,12 +7,14 @@ import { FaTimes } from "react-icons/fa";
 
 
 const DeleteButton = ({ workout }: { workout: IWorkout }) => {
-    const { todayPlan, setTodayPlan, saved, setSaved, selectedTab } = useContext(WorkoutContext)
+    const { todayPlan, setTodayPlan, saved, setSaved, selectedTab, markAsDone, setMarkAsDone } = useContext(WorkoutContext)
 
     const handleDelete = () => {
         if (selectedTab==='today-plan') {
             const remainingTodayPlan = todayPlan.filter(p => p.id !== workout.id)
             setTodayPlan(remainingTodayPlan)
+            const remainingMarkAsDone = markAsDone.filter(m=>m!==workout.id)
+            setMarkAsDone(remainingMarkAsDone)
         } else {
             const remainingSaved = saved.filter(s => s.id !== workout.id)
             setSaved(remainingSaved)
