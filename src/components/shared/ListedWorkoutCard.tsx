f8b17d7eddc84c0f53dad81dad64e4b22955427b
rezasdsju <rezaul2002104@gmail.com>
2026-09-25 07:31:07 +0600

    fix: show mark as done button only in today's plan

--- src/components/shared/ListedWorkoutCard.tsx
+++ src/components/shared/ListedWorkoutCard.tsx
@@ -6,9 +6,13 @@ import { FaFireFlameCurved } from "react-icons/fa6";
 import DeleteButton from "../deleteButton/DeleteButton";
 import Link from "next/link";
 import MarkAsDone from "./MarkAsDone";
+import { useContext } from "react";
+import { WorkoutContext } from "@/context/WorkoutContext";
 
 
 const ListedWorkoutCard = ({ workout }: { workout: IWorkout }) => {
+    const {selectedTab} = useContext(WorkoutContext)
+    
     return (
         <div className="sm:flex sm:justify-between bg-[#232732] border border-[#8A92A0] px-4 py-2 my-2 rounded-2xl">
             <div className="flex gap-4">
@@ -40,7 +44,9 @@ const ListedWorkoutCard = ({ workout }: { workout: IWorkout }) => {
             </div>
             <div className="flex items-center justify-center gap-4 pt-3 sm:pt-0">
                 <Link href={`/workout-details/${workout.id}`}><button className="bg-[#374151] text-[#FFFFFF] px-3 py-1 rounded-xl border border-[#8A92A0]">View Details</button></Link>
-                <MarkAsDone workout={workout}></MarkAsDone>
+                {
+                    selectedTab==='today-plan' && <MarkAsDone workout={workout}></MarkAsDone>
+                }
                 <DeleteButton workout={workout}></DeleteButton>
             </div>
         </div>
