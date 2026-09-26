f53f08bb30297af22c5196b379555e6346cb8a5d
rezasdsju <rezasdsju@gmail.com>
2026-09-26 19:14:49 +0600

    style: add padding

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@
 
 const GlobalLoading = () => {
     return (
-        <div className="flex items-center justify-center pt-20">
+        <div className="flex items-center justify-center pt-20 pb-200">
             <span className="loading loading-spinner text-secondary"></span>
 
         </div>
