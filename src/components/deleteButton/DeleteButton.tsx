e106379645a8864867b21d77fc0cf70cce63db0d
rezasdsju <rezasdsju@gmail.com>
2026-09-26 19:41:53 +0600

    feat: add toast notification when removing workout

--- src/components/deleteButton/DeleteButton.tsx
+++ src/components/deleteButton/DeleteButton.tsx
@@ -4,24 +4,20 @@ import { WorkoutContext } from "@/context/WorkoutContext";
 import { IWorkout } from "@/types/exerciseDataTypes";
 import { useContext } from "react";
 import { FaTimes } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 
 const DeleteButton = ({ workout }: { workout: IWorkout }) => {
-    // const { todayPlan, setTodayPlan, saved, setSaved, selectedTab, markAsDone, setMarkAsDone } = useContext(WorkoutContext)
     const { setTodayPlan, setSaved, selectedTab,  setMarkAsDone } = useContext(WorkoutContext)
 
     const handleDelete = () => {
         if (selectedTab==='today-plan') {
-            // const remainingTodayPlan = todayPlan.filter(p => p.id !== workout.id)
-            // setTodayPlan(remainingTodayPlan)
             setTodayPlan(prev=>prev.filter(p=>p.id!==workout.id))
-            // const remainingMarkAsDone = markAsDone.filter(m=>m!==workout.id)
-            // setMarkAsDone(remainingMarkAsDone)
             setMarkAsDone(prev=>prev.filter(m=>m!==workout.id))
+            toast.info("Workout removed from today's plan!");
         } else {
-            // const remainingSaved = saved.filter(s => s.id !== workout.id)
-            // setSaved(remainingSaved)
             setSaved(prev=>prev.filter(s => s.id !== workout.id))
+            toast.info('Workout removed from saved plan!');
         }
     }
     return (
