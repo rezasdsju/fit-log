9c30b7a09184bb626e35290914fd43296e058b0c
rezasdsju <rezasdsju@gmail.com>
2026-09-26 07:32:11 +0600

    fix: remove footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Banner from "@/components/homepage/Banner";
 import Library from "@/components/homepage/Library";
-import Footer from "@/components/shared/Footer";
+
 const page = () => {
   return (
     <div>
       <Banner></Banner>
       <Library></Library>
-      <Footer></Footer>
     </div>
   );
 };
